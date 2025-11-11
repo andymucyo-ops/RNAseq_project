548df1a84a1f5820f23a4c9b4395825161ad44cd
Andy Nkunzimana <ankunzimana@login8.hpc.binf.unibe.ch>
2025-11-11 17:02:09 +0100

    mapping scripts created

---
+++ scripts/mapping.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# set paths
+
+INPUT_DIR="/data/users/ankunzimana/RNAseq_project/data/reads_Blood"
+SCRIPT_DIR="/data/users/ankunzimana/RNAseq_project/scripts"
+
+for file_1 in `ls -l ${INPUT_DIR}/*_1.fastq.gz`; do 
+    file_2=${file_1%_1.fastq.gz}_2.fastq.gz
+    sample_name=$(basename ${file_1%_1.fastq.gz})
+    sbatch ${SCRIPT_DIR}/mapping.slurm $file_1 $file_2 $sample_name
+done
