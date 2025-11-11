dc1b74f3a2e72b760f97a443a769bf62b003820b
Andy Nkunzimana <ankunzimana@login8.hpc.binf.unibe.ch>
2025-11-12 00:57:10 +0100

    samtools script to convert to bam, sort and index bam files

---
+++ scripts/samtools.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+input_dir="/data/users/ankunzimana/RNAseq_project/results/alignement"
+
+script_dir="/data/users/ankunzimana/RNAseq_project/scripts"
+
+for sam_file in `ls -1 ${input_dir}/sam/*.sam`; do
+    bam_file= "${input_dir}/bam/$(basename ${sam_file%.sam}).bam"
+    sorted_bam= "${input_dir}/bam/$(basename ${sam_file%.sam}).sorted.bam"
+
+    # sbatch ${script_dir}/samtools.slurm ${sam_file} ${bam_file} ${sorted_bam}
+    echo "${sam_file} ${bam_file} ${sorted_bam}"
+done
