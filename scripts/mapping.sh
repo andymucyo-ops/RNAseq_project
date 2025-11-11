9941f3d67be0d702c0c37a847a04b37d9839e066
Andy Nkunzimana <ankunzimana@login8.hpc.binf.unibe.ch>
2025-11-11 23:21:54 +0100

    added documentation in script code

--- scripts/mapping.sh
+++ scripts/mapping.sh
@@ -5,6 +5,8 @@
 INPUT_DIR="/data/users/ankunzimana/RNAseq_project/data/reads_Blood"
 SCRIPT_DIR="/data/users/ankunzimana/RNAseq_project/scripts"
 
+#apply mapping.slurm script on all raw files 
+
 for file_1 in `ls -1 ${INPUT_DIR}/*_1.fastq.gz`; do 
     file_2=${file_1%_1.fastq.gz}_2.fastq.gz
     sample_name=$(basename ${file_1%_1.fastq.gz})
