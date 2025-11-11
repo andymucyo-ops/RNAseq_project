8064428e9996d6fe3a66482b214e65ce8d12450f
Andy Nkunzimana <ankunzimana@login8.hpc.binf.unibe.ch>
2025-11-11 20:02:46 +0100

    modified pathway for containers and files vairable in loop

--- scripts/mapping.sh
+++ scripts/mapping.sh
@@ -5,8 +5,8 @@
 INPUT_DIR="/data/users/ankunzimana/RNAseq_project/data/reads_Blood"
 SCRIPT_DIR="/data/users/ankunzimana/RNAseq_project/scripts"
 
-for file_1 in `ls -l ${INPUT_DIR}/*_1.fastq.gz`; do 
+for file_1 in `ls -1 ${INPUT_DIR}/*_1.fastq.gz`; do 
     file_2=${file_1%_1.fastq.gz}_2.fastq.gz
     sample_name=$(basename ${file_1%_1.fastq.gz})
-    sbatch ${SCRIPT_DIR}/mapping.slurm $file_1 $file_2 $sample_name
+    sbatch ${SCRIPT_DIR}/mapping.slurm ${file_1} ${file_2} ${sample_name}
 done
