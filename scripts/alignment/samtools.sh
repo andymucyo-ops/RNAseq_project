371c239fc2e46223563300cb25563f53547cd9e7
Andy Nkunzimana <ankunzimana@login8.hpc.binf.unibe.ch>
2025-11-12 19:48:36 +0100

    improved script documentation

--- scripts/alignment/samtools.sh
+++ scripts/alignment/samtools.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
+# setting pathways
+
 input_dir="/data/users/ankunzimana/RNAseq_project/results/alignment/sam"
 
 script_dir="/data/users/ankunzimana/RNAseq_project/scripts/alignment"
 
 output_dir="/data/users/ankunzimana/RNAseq_project/results/alignment/bam"
 
+#apply samtools.slurm on every sam file 
 for sam_file in `ls -1 ${input_dir}/*.sam`; do
 
     sample_name=$(basename ${sam_file%.sam})
-    # echo "${sam_file} ${output_dir}/${sample_name}"
-    sbatch ${script_dir}/samtools.slurm ${sam_file} "${output_dir}/${sample_name}"
+
+    bam_file="${output_dir}/${sample_name}.bam"
+
+    touch ${output_dir}/${sample_name}.sorted.bam
+
+    sorted_bam="${output_dir}/${sample_name}.sorted.bam"
+    # echo "${sam_file} ${bam_file} ${sorted_bam}"
+    sbatch ${script_dir}/samtools.slurm ${sam_file} ${bam_file} ${sorted_bam}
 done
