81284731f4aabfd1d9cbf761a073c57f9ec594ee
a-nkunzimana <andymucyo@gmail.com>
2026-01-20 21:34:29 +0100

    fixed colData according to readme

--- scripts/R_scripts/DES_data_analysis.R
+++ scripts/R_scripts/DES_data_analysis.R
@@ -12,101 +12,71 @@ library("enrichplot")
 #set wroking directory
 setwd("~/Projects/R/RNAseq_project/")
 
-# ------------------------------------------------------------------------------
+# ---------------------------------------------------------------------
 # 1) Data formatting before analysis
-# ------------------------------------------------------------------------------
+# ---------------------------------------------------------------------
+
 # set file path
 sample_counts_path <- "~/Projects/R/RNAseq_project/data/count.txt"
 
-
 # save the feature count file as a table
+counts <- read.table(sample_counts_path, header = TRUE)
 
-countdata <- read.table(sample_counts_path, header = TRUE)
-head(countdata)
+# changing rownames to gene id
+rownames(counts) <- counts$Geneid
 
-
-# changing the count into gene id
-
-rownames(countdata) <- countdata$Geneid
-countdata <- countdata[, -c(1)] #remove the gene id column
-head(countdata)
+#remove Geneid column
+counts$Geneid <- NULL
+head(counts)
 
 
-# Create a coldata data frame containing the data information according to
-# README in reads_Blood, acting as the metadata of the counts
+# Create a sample_info data frame containing the Data information according to README in reads_Blood, acting as the metadata of the counts
 
-coldata <- data.frame(
-  Sample = c(
-    "SRR7821949",
-    "SRR7821950",
-    "SRR7821951",
-    "SRR7821952",
-    "SRR7821953",
-    "SRR7821968",
-    "SRR7821969",
-    "SRR7821970",
-    "SRR7821954",
-    "SRR7821955",
-    "SRR7821956",
-    "SRR7821957",
-    "SRR7821971",
-    "SRR7821972",
-    "SRR7821973"
-  ),
-  Genotype = c(rep("WT", 8), rep("DKO", 7)),
-  Condition = c(
-    rep("Case", 5),
-    rep("Control", 3),
-    rep("Case", 4),
-    rep("Control", 3)
-  ),
-  row.names = NULL
-)
+sample_info_path <- "~/Projects/R/RNAseq_project/data/sample_info.txt"
 
-head(coldata, n = 10L)
+sample_info <- read.table(sample_info_path, header = TRUE)
 
-# converting sample into factor to match DESeq2 input format, and order by
-# the sample names to match column names of the count data table
-coldata$Sample <- factor(coldata$Sample, levels = sort(unique(coldata$Sample)))
-# saving the sorted meta data as a new data frame
-sorted_coldata <- coldata[order(coldata$Sample), ]
+#order sample_info acording to sample name
+sample_info <- sample_info[order(sample_info$Sample), ]
 
-# setting sample names as rownames
-rownames(sorted_coldata) <- sorted_coldata$Sample
+#change rownames to sample names
+rownames(sample_info) <- sample_info$Sample
 
-# remonving sample names column
-sorted_coldata <- sorted_coldata[, -c(1)]
+#remove sample column
+sample_info$Sample <- NULL
+head(sample_info)
 
-head(sorted_coldata, n = 10L)
+# final check before launching DESeq (Must return TRUE to perform DESeq analysis!)
+all(rownames(sample_info) == colnames(counts))
 
-# final check before launching DESeq
-all(rownames(sorted_coldata) == colnames(countdata))
 
-#-------------------------------------------------------------------------------
+#-----------------------------------------------------------------------
 # 2) start Differential gene expression analysis
-#-------------------------------------------------------------------------------
-# create DESeq data set from the feature counts data and the counts meta data
+#-----------------------------------------------------------------------
 
+# create DESeq data set from the feature counts data and the counts meta data(sample_info)
 dds <- DESeqDataSetFromMatrix(
-  countdata,
-  sorted_coldata,
-  design = ~ Genotype + Condition + Genotype:Condition
+  counts,
+  sample_info,
+  design = ~Group
 )
 
-#transform the meta data values into factor, to match DESeq expected data types, and set references for downstream data analysis
+# change Group data type to match expected type in DESeq() function
+dds$Group <- as.factor(dds$Group)
 
-dds$Genotype <- relevel(dds$Genotype, ref = "WT")
-dds$Condition <- relevel(dds$Condition, ref = "Control")
 
 # run differential expression analysis
 dds <- DESeq(dds)
 
-#-------------------------------------------------------------------------------
+#-----------------------------------------------------------------------
 # 3) Results and data visualization
-#-------------------------------------------------------------------------------
-# extraction of transformed values, and principal component plot of the samples
-vsd <- vst(dds, blind = FALSE) # extracts the corrected counts for each entry of the differential expression matrix
+#-----------------------------------------------------------------------
 
+# extraction of transformed values (corrected counts) of counts into new matrix to nromalize counts and compare them
+vsd <- vst(dds, blind = FALSE)
+
+
+#plot PCA (principal component analysis) to check for clustering of the data, and save as imageb file
 png(
   filename = "./results/R_plots/PCAplot.png",
   width = 3000,
@@ -114,90 +84,115 @@ png(
   res = 300
 )
 
-plotPCA(vsd, intgroup = c("Genotype", "Condition"), ntop = 500, ) # visualization plot of the clustering of the data
+plotPCA(
+  vsd,
+  intgroup = c("Group"),
+  ntop = 500,
+)
 
 dev.off()
 
-# get results coefficient to use for highlight in the following result step
-resultsNames(dds)
+#-----------------------------------------------------------------------
+# get results comparison with 4 different methods
+#-----------------------------------------------------------------------
+#Highlight differences of expression between case and control condition in WT genotype
+res_WT <- results(
+  dds,
+  contrast = c(
+    "Group",
+    "Blood_WT_Case",
+    "Blood_WT_Control"
+  ),
+  alpha = 0.05
+)
 
-# get results comparison with 3 different methods
-res_WT <- results(dds, name = "Condition_Case_vs_Control", alpha = 0.05)
-#Highlight differences of expression between control and case in WT
-
+# highlight of the difference of expression between case and control condition in DKO genotype
 res_DKO <- results(
   dds,
-  contrast = list(c(
-    "GenotypeDKO.ConditionCase",
-    "Condition_Case_vs_Control"
-  )),
+  contrast = c(
+    "Group",
+    "Blood_DKO_Case",
+    "Blood_DKO_Control"
+  ),
   alpha = 0.05
 )
-# highlight of the difference of expression between control and case in DKO
 
+# highlight the difference of expression in control condition between WT and DKO in Case condition
 res_DKOvsWT_case <- results(
   dds,
-  name = "GenotypeDKO.ConditionCase",
+  contrast = c(
+    "Group",
+    "Blood_DKO_Case",
+    "Blood_WT_Case"
+  ),
   alpha = 0.05
 )
 
-# highlight the response of the genes in control condition between WT and DKO
-# enables for verification of the genes for which the response to the condition depends on the Genotype
+# highlight the difference of expression in control condition between WT and DKO in control condition
+res_DKOvsWT_control <- results(
+  dds,
+  contrast = c(
+    "Group",
+    "Blood_DKO_Control",
+    "Blood_WT_Control"
+  ),
+  alpha = 0.05
+)
 
 # get summary of the result data output for each comparison method
-
 summary_WT <- summary(res_WT)
 summary_DKO <- summary(res_DKO)
 summary_DKOvsWT_case <- summary(res_DKOvsWT_case)
+summary_DKOvsWT_control <- summary(res_DKOvsWT_control)
 
 
-# get the significant genes for each result method, then reduce it to the top 50 differentialy expressed genes
+#Create function to retrieve top 50 genes for each result sets
+get_top_genes <- function(res) {
+  #set filtering criteria for the results with padj defined and absolute value of log2FoldChange > 1
+  filtering_criteria <- which(
+    !is.na(res$padj) &
+      abs(res$log2FoldChange) > 1
+  )
 
-#set filtering criteria for each result set
-filtering_criteria <- which(
-  !is.na(res_WT$padj) &
-    res_WT$padj < 0.05 &
-    abs(res_WT$log2FoldChange) > 1
-)
+  # get significant genes order by padj value
+  sig_genes <- rownames(res)[filtering_criteria][order(res$padj[
+    filtering_criteria
+  ])]
 
-# WT
-sig_genes_WT <- rownames(res_WT)[filtering_criteria][order(res_WT$padj[
-  filtering_criteria
-])]
-filtered_counts_WT <- length(sig_genes_WT) # nmbr of genes after filtering: 6768
-top200_genes_WT <- sig_genes_WT[1:200]
+  top50_genes <- sig_genes[1:50]
 
-# DKO
-
-sig_genes_DKO <- rownames(res_DKO)[filtering_criteria][order(res_DKO$padj[
-  filtering_criteria
-])]
-filtered_counts_DKO <- length(sig_genes_DKO) # nmbr of genes after filtering: 5450
-top200_genes_DKO <- sig_genes_DKO[1:200]
-
-# DKO vs WT
-sig_genes_DKOvsWT_case <- rownames(res_DKOvsWT_case)[
-  filtering_criteria
-][order(res_DKOvsWT_case$padj[filtering_criteria])]
-filtered_counts_DKOvsWT <- length(sig_genes_DKOvsWT_case) # nmbr of genes after filtering: 3745
-top200_genes_DKOvsWT_case <- sig_genes_DKOvsWT_case[1:200]
+  return(top50_genes)
+}
 
 
-# extract the transformed count matrix matching the top 50 genes for each results highlight
-mat_WT <- assay(vsd)[top200_genes_WT, ]
-mat_DKO <- assay(vsd)[top200_genes_DKO, ]
-mat_DKOvsWT_case <- assay(vsd)[top200_genes_DKOvsWT_case, ]
+#-----------------------------------------------------------------------
+#get to 50 genes for each comparison
+#-----------------------------------------------------------------------
+top50_WT <- get_top_genes(res_WT)
 
+top50_DKO <- get_top_genes(res_DKO)
 
+top50_DKOvsWT_case <- get_top_genes(res_DKOvsWT_case)
+
+top50_DKOvsWT_control <- get_top_genes(res_DKOvsWT_control)
+
+# extract the corrected count matrix matching the top 50 genes for each results highlight
+mat_WT <- assay(vsd)[top50_WT, ]
+mat_DKO <- assay(vsd)[top50_DKO, ]
+mat_DKOvsWT_case <- assay(vsd)[top50_DKOvsWT_case, ]
+mat_DKOvsWT_control <- assay(vsd)[top50_DKOvsWT_control, ]
+
+#---------------------------------------------------------------------
 # heatmap for different highlighted result data frame
+#---------------------------------------------------------------------
 pheatmap(
   mat_WT,
   scale = "row",
   main = "Case vs Control condition comparison in WT genotype",
-  annotation_col = as.data.frame(colData(dds)[, c(
-    "Genotype",
-    "Condition"
-  )]),
+  annotation_col = data.frame(
+    Group = colData(dds)$Group,
+    row.names = colnames(mat_WT)
+  ),
   show_rownames = FALSE,
   cluster_rows = TRUE,
   cluster_cols = TRUE,
@@ -210,10 +205,10 @@ pheatmap(
   mat_DKO,
   scale = "row",
   main = "Case vs Control condition comparison in DKO genotype",
-  annotation_col = as.data.frame(colData(dds)[, c(
-    "Genotype",
-    "Condition"
-  )]),
+  annotation_col = data.frame(
+    Group = colData(dds)$Group,
+    row.names = colnames(mat_DKO)
+  ),
   show_rownames = FALSE,
   cluster_rows = TRUE,
   cluster_cols = TRUE,
@@ -226,10 +221,10 @@ pheatmap(
   mat_DKOvsWT_case,
   scale = "row",
   main = "DKO vs WT in case condition",
-  annotation_col = as.data.frame(colData(dds)[, c(
-    "Genotype",
-    "Condition"
-  )]),
+  annotation_col = data.frame(
+    Group = colData(dds)$Group,
+    row.names = colnames(mat_DKOvsWT_case)
+  ),
   show_rownames = FALSE,
   cluster_rows = TRUE,
   cluster_cols = TRUE,
@@ -238,9 +233,27 @@ pheatmap(
   filename = "./results/R_plots/heatmap_DKOvsWT_case.png"
 )
 
-# volcano plot for each result set and save them as png files
+pheatmap(
+  mat_DKOvsWT_control,
+  scale = "row",
+  main = "DKO vs WT in control condition",
+  annotation_col = data.frame(
+    Group = colData(dds)$Group,
+    row.names = colnames(mat_DKOvsWT_control)
+  ),
+  show_rownames = FALSE,
+  cluster_rows = TRUE,
+  cluster_cols = TRUE,
+  fontsize = 10,
+  fontsize_row = 8,
+  filename = "./results/R_plots/heatmap_DKOvsWT_control.png"
+)
 
-# Genotype DKO vs WT volcano plot
+#-----------------------------------------------------------------------
+# volcano plot for each result set and save them as png files
+#-----------------------------------------------------------------------
+
+# Case vs Control condition comparison in WT genotype
 png(
   filename = "./results/R_plots/volcano_plot_WT.png",
   width = 3000,
@@ -259,7 +272,7 @@ EnhancedVolcano(
 
 dev.off()
 
-# Condition case vs control volcano plot
+# Case vs Control condition comparicon in DKO Genotype
 png(
   filename = "./results/R_plots/volcano_plot_DKO.png",
   width = 3000,
@@ -278,7 +291,7 @@ EnhancedVolcano(
 
 dev.off()
 
-# Interaction volcano plot
+# DKO vs WT genotype comparison in Case condition
 png(
   filename = "./results/R_plots/volcano_plot_DKOvsWT_case.png",
   width = 3000,
@@ -292,31 +305,55 @@ EnhancedVolcano(
   x = 'log2FoldChange',
   y = 'padj',
   title = 'Condition effect: DKO vs WT',
-  subtitle = 'Differential response in Control control condition'
+  subtitle = 'Differential response in Case condition'
 )
 
 dev.off()
 
-# ------------------------------------------------------------------------------
+
+# Interaction volcano plot
+png(
+  filename = "./results/R_plots/volcano_plot_DKOvsWT_control.png",
+  width = 3000,
+  height = 2400,
+  res = 300
+)
+
+EnhancedVolcano(
+  res_DKOvsWT_case,
+  lab = rownames(res_DKOvsWT_control),
+  x = 'log2FoldChange',
+  y = 'padj',
+  title = 'Condition effect: DKO vs WT',
+  subtitle = 'Differential response in Control condition'
+)
+
+dev.off()
+
+# -----------------------------------------------------------------------
 # 4) enrichGO analysis
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
 # save gene list of differentialy expressed genes, for each results data
-gene_list_WT <- sig_genes_WT
+gene_list_WT <- top50_WT
 
-gene_list_DKO <- sig_genes_DKO
+gene_list_DKO <- top50_DKO
 
-gene_list_DKOvsWT_case <- sig_genes_DKOvsWT_case
+gene_list_DKOvsWT_case <- top50_DKOvsWT_case
 
-
+gene_list_DKOvsWT_control <- top50_DKOvsWT_control
 # set universe list (ensemble of all genes), cf. summary(res)
 
 universe_WT <- rownames(res_WT)[which(!is.na(res_WT$padj))]
 
 universe_DKO <- rownames(res_DKO)[which(!is.na(res_DKO$padj))]
 
-universe_DKOvsWT <- rownames(res_DKOvsWT_case)[which(
+universe_DKOvsWT_case <- rownames(res_DKOvsWT_case)[which(
   !is.na(res_DKOvsWT_case$padj)
+)]
+
+universe_DKOvsWT_control <- rownames(res_DKOvsWT_control)[which(
+  !is.na(res_DKOvsWT_control$padj)
 )]
 
 
@@ -344,29 +381,41 @@ ego_DKO <- enrichGO(
 
 head(ego_DKO, 3)
 
-ego_DKOvsWT <- enrichGO(
+ego_DKOvsWT_case <- enrichGO(
   gene = gene_list_DKOvsWT_case,
-  universe = universe_DKOvsWT,
+  universe = universe_DKOvsWT_case,
   OrgDb = org.Mm.eg.db,
   ont = "BP",
   keyType = "ENSEMBL",
   readable = TRUE
 )
 
-head(ego_DKOvsWT, 3)
+head(ego_DKOvsWT_case, 3)
 
+ego_DKOvsWT_control <- enrichGO(
+  gene = gene_list_DKOvsWT_control,
+  universe = universe_DKOvsWT_control,
+  OrgDb = org.Mm.eg.db,
+  ont = "BP",
+  keyType = "ENSEMBL",
+  readable = TRUE
+)
+
+head(ego_DKOvsWT_control, 3)
+
+#-----------------------------------------------------------------------
 # ego's bar plots
-
+#-----------------------------------------------------------------------
 png(
   filename = "./results/R_plots/barplot_enrichGO_WT.png",
   width = 3000,
   height = 2400,
   res = 300
 )
-barplot(ego_WT, showCategory = 10) +
+barplot(ego_WT, x = "Count") +
   ggtitle(
     "Enriched terms bar plot",
-    "In WT: in case vs control comparison"
+    "In WT: case vs control comparison"
   )
 dev.off()
 
@@ -377,18 +426,32 @@ png(
   height = 2400,
   res = 300
 )
-barplot(ego_DKO, showCategory = 8) +
+barplot(ego_DKO, x = "Count") +
   ggtitle("Enriched terms bar plot", "In DKO: case vs control comparison")
 dev.off()
 
 
 png(
-  filename = "./results/R_plots/barplot_enrichGO_DKOvsWT.png",
+  filename = "./results/R_plots/barplot_enrichGO_DKOvsWT_case.png",
   width = 3000,
   height = 2400,
   res = 300
 )
-barplot(ego_DKOvsWT, showCategory = 10) +
+barplot(ego_DKOvsWT_case, x = "Count") +
+  ggtitle(
+    "Enriched terms bar plot",
+    "In Case condition: DKO vs WT comparison"
+  )
+dev.off()
+
+
+png(
+  filename = "./results/R_plots/barplot_enrichGO_DKOvsWT_control.png",
+  width = 3000,
+  height = 2400,
+  res = 300
+)
+barplot(ego_DKOvsWT_control, x = "Count") +
   ggtitle(
     "Enriched terms bar plot",
     "In Control condition: DKO vs WT comparison"
